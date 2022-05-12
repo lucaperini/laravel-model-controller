22292f7c5949be897a480987d671173b4979be13
lucaperini <luca.perini@gmail.com>
2022-05-12 15:45:54 +0200

    first commit

---
+++ laravel-model-controller/resources/views/layouts/base.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+    <title>Laravel-Model-Controller - @yield('title')</title>
+
+    @yield('head-scripts')
+
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+</head>
+<body>
+    @include('partials.header')
+
+    <main>
+        @yield('main-content')
+    </main>
+
+    @include('partials.footer')
+    @include('partials.social')
+
+    @yield('footer-scripts')
+</body>
+</html>
